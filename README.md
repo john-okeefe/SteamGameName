d21f6d2bc584eb8c4ae11ed3d8acc4797b5726dc
John O'Keefe <jokeefe@fastmail.com>
2023-12-31 16:26:18 -0500

    added directory checks

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Shell script to show the name of games in the compdata directory.
 
-For now, the script works. However, it does not have any error checks. Use at your own risk.
+For now, the script works. However, it only does directory checks. Use at your own risk.
--- steamgamename.sh
+++ steamgamename.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
 steamappsPath=$1
+steamappsRegex='^.*steamapps\/?\r?\n?'
 
-if [ "$steamappsPath" == "" ]
-then
+if [ "$steamappsPath" == "" ]; then
   echo "Usage: $0 [ \"steamapps directory\" ]"
   echo "This program will look through all compdata folders"
   echo "and give you the Steam Titles."
   exit
 fi
 
-for dir in "$steamappsPath"/compatdata/*
-do
-  isInstalled=""
-  steamId=$(basename "$dir")
-  steamId=${steamId%\n}
-  title=$(curl -s "https://store.steampowered.com/api/appdetails/?appids=$steamId" | jq ".[].data.name")
-  if test -f "../appmanifest_$steamId.acf"
-  then
-    isInstalled="[ installed ]"
-  fi
+if ! [[ "$steamappsPath" =~ $steamappsRegex ]]; then
+  echo "Path should end with steamapps"
+  exit
+fi
 
-  echo "$steamId: $title $isInstalled"
-done
+if test -d "$steamappsPath"; then
+  for dir in "$steamappsPath"/compatdata/*; do
+    isInstalled=""
+    steamId=$(basename "$dir")
+    steamId=${steamId%\n}
+    title=$(curl -s "https://store.steampowered.com/api/appdetails/?appids=$steamId" | jq ".[].data.name")
+    if test -f "$steamappsPath/appmanifest_$steamId.acf"; then
+      isInstalled="[Installed]"
+    fi
+
+    echo "$steamId: $title $isInstalled"
+  done
+else
+  echo "Please enter a valid steamapps directory"
+fi
